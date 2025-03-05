1768f2fd32fa35ebfc7b5c0aef970aa0c35f2951
FrhnSwpli <andifarhansappewali407@gmail.com>
2025-03-05 13:56:41 +0800

    add icons

--- config/skillsConfig.ts
+++ config/skillsConfig.ts
@@ -1,16 +1,20 @@
-import { FaPhp, FaJs, FaHtml5, FaCss3Alt, FaGitAlt, FaCloud } from "react-icons/fa";
-import { SiMysql, SiTailwindcss, SiFirebase, SiGooglecloud } from "react-icons/si";
+import { FaPhp, FaJs, FaPython, FaHtml5, FaCss3Alt, FaGitAlt, FaCloud, FaReact } from "react-icons/fa";
+import { SiMysql, SiPostgresql, SiTailwindcss, SiFirebase, SiGooglecloud, SiNextdotjs, SiTypescript } from "react-icons/si";
 import { DiLaravel } from "react-icons/di";
 
 export const skillsConfig = [
   { icon: FaPhp, label: "PHP" },
+  { icon: FaPython, label: "Python" }, 
   { icon: FaJs, label: "JavaScript" },
+  { icon: SiTypescript, label: "TypeScript" },
   { icon: FaHtml5, label: "HTML5" },
   { icon: FaCss3Alt, label: "CSS3" },
-  { icon: FaGitAlt, label: "Git" },
-  { icon: SiMysql, label: "MySQL" },
   { icon: SiTailwindcss, label: "Tailwind CSS" },
+  { icon: FaGitAlt, label: "Git" },
+  { icon: FaReact, label: "React" },
+  { icon: SiNextdotjs, label: "Next.js" },
   { icon: DiLaravel, label: "Laravel" },
+  { icon: SiMysql, label: "MySQL" },
   { icon: SiFirebase, label: "Firebase" },
   { icon: FaCloud, label: "Cloud Computing" },
   { icon: SiGooglecloud, label: "Google Cloud Platform" },
